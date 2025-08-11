1aa5b55a62088dd28e832489d603ff906487d463
tcsenpai <dev@tcsenpai.com>
2025-08-11 12:14:54 +0200

    no cache in run

--- run.sh
+++ run.sh
@@ -2,11 +2,16 @@
 
 BUILD_FLAG=""
 DETACH_FLAG=""
+NO_CACHE_FLAG=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     -b|--build)
       BUILD_FLAG="--build"
+      shift
+      ;;
+    -n|--no-cache)
+      NO_CACHE_FLAG="--no-cache"
       shift
       ;;
     -d|--detach)
@@ -15,11 +20,19 @@ while [[ $# -gt 0 ]]; do
       ;;
     *)
       echo "Unknown option $1"
-      echo "Usage: $0 [-b|--build] [-d|--detach]"
+      echo "Usage: $0 [-b|--build] [-n|--no-cache] [-d|--detach]"
       exit 1
       ;;
   esac
 done
 
-echo "Starting services..."
-docker-compose up $BUILD_FLAG $DETACH_FLAG
+# If no-cache is specified, we need to build separately
+if [[ -n "$NO_CACHE_FLAG" ]]; then
+  echo "Building services with no cache..."
+  docker-compose build $NO_CACHE_FLAG
+  echo "Starting services..."
+  docker-compose up $DETACH_FLAG
+else
+  echo "Starting services..."
+  docker-compose up $BUILD_FLAG $DETACH_FLAG
+fi
